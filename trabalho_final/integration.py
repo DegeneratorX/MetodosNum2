7f0872460aea51fcc6238906b42ab5675cead510
Victor <victorio.deg@gmail.com>
2023-05-05 18:32:50 -0300

    Ola

--- trabalho_final/integration.py
+++ trabalho_final/integration.py
@@ -453,17 +453,25 @@ class GaussExponenciacao(Integracao):
                 x_inicial = a+particao*tamanho_particao
                 x_final = x_inicial+tamanho_particao
                 if grau == 1:
-                    soma = soma + cls.__grau_1(f, x_inicial, x_final, limite_inferior_a, limite_superior_b, exponencial_simples)
+                    soma = soma + \
+                        cls.__grau_1(f, x_inicial, x_final, limite_inferior_a,
+                                     limite_superior_b, exponencial_simples)
                 elif grau == 2:
-                    soma = soma + cls.__grau_2(f, x_inicial, x_final, limite_inferior_a, limite_superior_b, exponencial_simples)
+                    soma = soma + \
+                        cls.__grau_2(f, x_inicial, x_final, limite_inferior_a,
+                                     limite_superior_b, exponencial_simples)
                 elif grau == 3:
-                    soma = soma + cls.__grau_3(f, x_inicial, x_final, limite_inferior_a, limite_superior_b, exponencial_simples)
+                    soma = soma + \
+                        cls.__grau_3(f, x_inicial, x_final, limite_inferior_a,
+                                     limite_superior_b, exponencial_simples)
                 elif grau == 4:
-                    soma = soma + cls.__grau_4(f, x_inicial, x_final, limite_inferior_a, limite_superior_b, exponencial_simples)
+                    soma = soma + \
+                        cls.__grau_4(f, x_inicial, x_final, limite_inferior_a,
+                                     limite_superior_b, exponencial_simples)
                 else:
                     print("Grau inválido.")
                     return
-                
+
             if primeira_iteracao:
                 soma_anterior = 0
                 primeira_iteracao = False
@@ -479,9 +487,8 @@ class GaussExponenciacao(Integracao):
                 return soma_atual
             particoes *= 2
 
-
-
     # Função x(a_k) de gauss-legendre. Mudei para "s", faz mais sentido nesse caso.
+
     @staticmethod
     def __s(x_inicial, x_final, raiz_a_k):
         return ((x_inicial+x_final)/2) + ((x_final-x_inicial)/2) * raiz_a_k
@@ -490,7 +497,7 @@ class GaussExponenciacao(Integracao):
     @staticmethod
     def __x_de_s_simples(s, limite_inferior_a, limite_superior_b):
         return ((limite_inferior_a+limite_superior_b)/2) + ((limite_superior_b-limite_inferior_a)/2) * np.tanh(s)
-    
+
     def __x_de_s_dupla(s, limite_inferior_a, limite_superior_b):
         return ((limite_inferior_a+limite_superior_b)/2)+((limite_superior_b-limite_inferior_a))*np.tanh((np.pi/2)*np.sinh(s))
 
@@ -498,9 +505,9 @@ class GaussExponenciacao(Integracao):
     @classmethod
     def __f_barra_de_s(cls, s, f, limite_inferior_a, limite_superior_b, exponencial_simples):
         if exponencial_simples:
-            return f(cls.__x_de_s_simples(s,limite_inferior_a, limite_superior_b))*(((limite_superior_b - limite_inferior_a)/2)*(1/np.cosh(s)**2))
+            return f(cls.__x_de_s_simples(s, limite_inferior_a, limite_superior_b))*(((limite_superior_b - limite_inferior_a)/2)*(1/np.cosh(s)**2))
         else:
-            return f(cls.__x_de_s_dupla(s,limite_inferior_a, limite_superior_b))*(((limite_superior_b - limite_inferior_a)/2)*((np.pi/2)*(np.cosh(s)/((np.cosh((np.pi/2)*np.sinh(s)))**2))))
+            return f(cls.__x_de_s_dupla(s, limite_inferior_a, limite_superior_b))*(((limite_superior_b - limite_inferior_a)/2)*((np.pi/2)*(np.cosh(s)/((np.cosh((np.pi/2)*np.sinh(s)))**2))))
 
     # Gauss-Legendre grau 1
     @classmethod
@@ -508,8 +515,8 @@ class GaussExponenciacao(Integracao):
         x_1 = 0
         w_1 = 1
         return \
-            ((x_final-x_inicial)/2 * \
-            ((cls.__f_barra_de_s(cls.__s(x_inicial, x_final, x_1), f, limite_inferior_a, limite_superior_b, exponencial_simples))*w_1))
+            2*(((x_final-x_inicial)/2 *
+                ((cls.__f_barra_de_s(cls.__s(x_inicial, x_final, x_1), f, limite_inferior_a, limite_superior_b, exponencial_simples))*w_1)))
 
     # Gauss-Legendre grau 2
     @classmethod
@@ -518,9 +525,9 @@ class GaussExponenciacao(Integracao):
         x_2 = +np.sqrt(1/3)
         w_1 = w_2 = 1
         return \
-            ((x_final-x_inicial)/2 * \
-            ((cls.__f_barra_de_s(cls.__s(x_inicial, x_final, x_1), f, limite_inferior_a, limite_superior_b, exponencial_simples))*w_1 + \
-            (cls.__f_barra_de_s(cls.__s(x_inicial, x_final, x_2), f, limite_inferior_a, limite_superior_b, exponencial_simples))*w_2))
+            ((x_final-x_inicial)/2 *
+             ((cls.__f_barra_de_s(cls.__s(x_inicial, x_final, x_1), f, limite_inferior_a, limite_superior_b, exponencial_simples))*w_1 +
+             (cls.__f_barra_de_s(cls.__s(x_inicial, x_final, x_2), f, limite_inferior_a, limite_superior_b, exponencial_simples))*w_2))
 
     # Gauss-Legendre grau 3
     @classmethod
@@ -531,10 +538,10 @@ class GaussExponenciacao(Integracao):
         w_1 = w_3 = 5/9
         w_2 = 8/9
         return \
-            ((x_final-x_inicial)/2 * \
-            ((cls.__f_barra_de_s(cls.__s(x_inicial, x_final, x_1), f, limite_inferior_a, limite_superior_b, exponencial_simples))*w_1 + \
-            (cls.__f_barra_de_s(cls.__s(x_inicial, x_final, x_2), f, limite_inferior_a, limite_superior_b, exponencial_simples))*w_2 + \
-            (cls.__f_barra_de_s(cls.__s(x_inicial, x_final, x_3), f, limite_inferior_a, limite_superior_b, exponencial_simples))*w_3))
+            ((x_final-x_inicial)/2 *
+             ((cls.__f_barra_de_s(cls.__s(x_inicial, x_final, x_1), f, limite_inferior_a, limite_superior_b, exponencial_simples))*w_1 +
+             (cls.__f_barra_de_s(cls.__s(x_inicial, x_final, x_2), f, limite_inferior_a, limite_superior_b, exponencial_simples))*w_2 +
+             (cls.__f_barra_de_s(cls.__s(x_inicial, x_final, x_3), f, limite_inferior_a, limite_superior_b, exponencial_simples))*w_3))
 
     # Gauss-Legendre grau 4
     @classmethod
@@ -547,11 +554,12 @@ class GaussExponenciacao(Integracao):
         w_2 = w_3 = (1/36)*(18+np.sqrt(30))
 
         return \
-            ((x_final-x_inicial)/2 * \
-            ((cls.__f_barra_de_s(cls.__s(x_inicial, x_final, x_1), f, limite_inferior_a, limite_superior_b, exponencial_simples))*w_1 + \
-            (cls.__f_barra_de_s(cls.__s(x_inicial, x_final, x_2), f, limite_inferior_a, limite_superior_b, exponencial_simples))*w_2 + \
-            (cls.__f_barra_de_s(cls.__s(x_inicial, x_final, x_3), f, limite_inferior_a, limite_superior_b, exponencial_simples))*w_3 + \
-            (cls.__f_barra_de_s(cls.__s(x_inicial, x_final, x_4), f, limite_inferior_a, limite_superior_b, exponencial_simples))*w_4))
+            ((x_final-x_inicial)/2 *
+             ((cls.__f_barra_de_s(cls.__s(x_inicial, x_final, x_1), f, limite_inferior_a, limite_superior_b, exponencial_simples))*w_1 +
+             (cls.__f_barra_de_s(cls.__s(x_inicial, x_final, x_2), f, limite_inferior_a, limite_superior_b, exponencial_simples))*w_2 +
+             (cls.__f_barra_de_s(cls.__s(x_inicial, x_final, x_3), f, limite_inferior_a, limite_superior_b, exponencial_simples))*w_3 +
+             (cls.__f_barra_de_s(cls.__s(x_inicial, x_final, x_4), f, limite_inferior_a, limite_superior_b, exponencial_simples))*w_4))
+
 
 def h(x):
     return 1/((x**2)**(1/3))
@@ -565,4 +573,5 @@ if __name__ == '__main__':
     # NewtonCotes.integrar(f, a=0, b=1, grau=1, tipo=0)
     # GaussLegendre.integrar(f, a=0, b=1, grau=1)
     # GaussChebychev.integrar(f,grau=4)
-    GaussExponenciacao.integrar(h, limite_inferior_a=2, limite_superior_b=20, c=2, grau=4, exponencial_simples=True)
+    GaussExponenciacao.integrar(
+        h, limite_inferior_a=2, limite_superior_b=20, c=2, grau=1, exponencial_simples=True)
