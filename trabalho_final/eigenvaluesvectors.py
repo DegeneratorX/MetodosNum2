c5d0c3dde782d3c9f37db8638bab9d50718622bd
anonymous <anonymous@example.com>
2023-06-14 20:20:33 -0300

    Ola

--- trabalho_final/eigenvaluesvectors.py
+++ trabalho_final/eigenvaluesvectors.py
@@ -94,7 +94,7 @@ def main():
     vetor_de_deslocamentos_matriz_1 = dividir_intervalos(np.floor(minimo_matriz_1[0]), np.ceil(dominante_matriz_1[0]), tam_matriz=matriz_1.shape[0])
     vetor_de_deslocamentos_matriz_2 = dividir_intervalos(np.floor(minimo_matriz_2[0]), np.ceil(dominante_matriz_2[0]), tam_matriz=matriz_2.shape[0])
     vetor_de_deslocamentos_matriz_3 = dividir_intervalos(np.floor(minimo_matriz_3[0]), np.ceil(dominante_matriz_3[0]), tam_matriz=matriz_3.shape[0])
-    print(vetor_de_deslocamentos_matriz_1)
+
     print()
     # Matriz 1:
     print("============MATRIZ 1=============")
