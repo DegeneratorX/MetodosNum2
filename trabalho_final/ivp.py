556ee5ed69f6c80f7fe502b26370a5e54c6f7494
Victor <victorio.deg@gmail.com>
2023-07-10 23:35:53 -0300

    Ola

--- trabalho_final/ivp.py
+++ trabalho_final/ivp.py
@@ -1,33 +1,40 @@
 # Dupla: Victor Martins e Luiz Gustavo
 # Turma 2023.1
 
-import numpy as np
-from copy import deepcopy
-from collections import deque
-import matplotlib.pyplot as plt
+G = 9.8
 
 class ProblemaValorInicial:
+    # Euler explícito: calcula a solução do próximo passo de tempo através 
+    # do passo de tempo atual
     @classmethod
-    def euler_explicito(cls, y0, v0, k, massa, delta_t):
-        alturas = [y0]
-        tempos = [0]
-        altura_maxima = 0
-        tempo_altura_maxima = 0
-        i = 0
-        while alturas[i] > 0:
+    def euler_explicito(cls, delta_t, velocidade_inicial, altura_inicial, k, massa):
+        alturas = [altura_inicial] # lista de posições do objeto
+        velocidade_atual = velocidade_inicial # Vou precisar também da velocidade anterior
+        tempos = [0] # lista que guarda valores de tempos pontuais
+
+        altura_maxima = 0 # Maior altura atingida
+        tempo_altura_maxima = 0 # Tempo exato que essa altura foi atingida
+        i = 0 # Contador
+
+        while alturas[i] > 0: # Enquanto nenhuma altura listada atingiu o solo
             i += 1
-            alturas.append(alturas[i-1] + delta_t * v0)
-            tempos.append(delta_t * i)
-            velocidade_anterior = v0
-            v0 = v0 + delta_t * (-10 - (k/massa) * v0)
-            if velocidade_anterior*v0 < 0:
-                altura_maxima = alturas[i-1] + (delta_t/2) * v0
+            alturas.append(alturas[i-1] + velocidade_atual * delta_t) # Fórmula y0 + v0t. Pego a altura anterior para formar a nova
+            tempos.append(delta_t * i) # Cálculo do novo tempo
+
+            velocidade_anterior = velocidade_atual # Velocidade atual vira a anterior
+            velocidade_atual = velocidade_atual + delta_t * (-G - (k/massa) * velocidade_atual) # Cálculo do método de Euler Explícito F(S_0, t_0), como mostrado no doc
+
+             # Se o produto da velocidade anterior e atual for negativo, significa
+             # que o objeto atingiu a altura máxima.
+            if velocidade_anterior * velocidade_atual < 0:
+                altura_maxima = alturas[i-1] + velocidade_atual * (delta_t/2)
                 tempo_altura_maxima = tempos[i-1] + delta_t/2
 
-        alturas.append(tempos[i-1]+(delta_t/2)*v0)
-        tempos.append(tempos[i-1]+delta_t/2)
+        # Cálculo da posição e tempo final
+        alturas.append(alturas[i-1] + (delta_t/2) * velocidade_atual)
+        tempos.append(tempos[i-1] + delta_t/2)
 
-        return alturas[i+1], altura_maxima, v0, tempos[i+1], tempo_altura_maxima
+        return altura_maxima, velocidade_atual, tempos[i + 1], tempo_altura_maxima, i
 
 
 def main():
@@ -39,11 +46,12 @@ def main():
     for i in range(1,5):
         dt = 10 ** (-i)
         print('--'*8+f' delta t = {dt} '+'--'*8+'\n')
-        altura_valor, altura_maxima, v, tempo_total, tempo_altura_maxima = ProblemaValorInicial.euler_explicito(y0, v0, k, m, dt)
+        altura_maxima, v, tempo_total, tempo_altura_maxima, iteracoes = ProblemaValorInicial.euler_explicito(dt, v0, y0, k, m)
         print("Altura máxima da trajetória = ", altura_maxima ,"metros")
         print("Tempo decorrido até a altura máxima = ", tempo_altura_maxima, "segundos")
         print("Tempo total até a queda no mar = ", tempo_total, "segundos")
         print("velocidade no momento do impacto com o mar = ", v, "m/s")
+        print(f"numero de iterações: {iteracoes}")
 
 
 if __name__ == "__main__":
